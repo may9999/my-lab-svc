9e08746eb8445bc0396387cc7a0c6b4a751fa04b
Ismael Ruiz Moya <ing_ismael_ruiz@hotmail.com>
2024-04-24 10:42:18 -0600

    edit clinical study

--- routes/clinical-studies.js
+++ routes/clinical-studies.js
@@ -116,87 +116,60 @@ router.get('', async (req, resp) => {
 //     } 
 // });
 
-// // UPDATE A USER
-// // Only admins can update users.. in future is we want 
-// // users can modify his own user then we have change the logic
-// router.patch('/:id', async (req, resp) => {
-//     const currentUser = req.headers['current-user'];
-//     isAdmin = false;
+// UPDATE A CLINICAL STUDY
+// Only admins can update
+router.patch('/:id', async (req, resp) => {
+    const currentUser = req.headers['current-user'];
+    isAdmin = false;
 
-//     ///////////THIS BLOCQ MUST BE REUSABLE
-//     try {
-//         const user = await User.findById(currentUser).select("-password");
-//         isAdmin = user.role === 'ADMIN' ? true : false;
-//     } catch(err) {
-//         return resp.json({ message: 'Invalid ID' });
-//     }
+    ///////////THIS BLOCQ MUST BE REUSABLE
+    try {
+        const user = await User.findById(currentUser).select("-password");
+        isAdmin = user.role === 'ADMIN' ? true : false;
+    } catch(err) {
+        return resp.json({ message: 'Invalid ID' });
+    }
 
-//     if (!isAdmin) {
-//         return resp.status(401).json({ message: 'Unauthorized' });
-//     }
-//     ///////////// FIN THIS BLOCQ MUST BE REUSABLE
+    if (!isAdmin) {
+        return resp.status(401).json({ message: 'Unauthorized' });
+    }
+    ///////////// FIN THIS BLOCQ MUST BE REUSABLE
 
-//     // Validate DATA before to be inserted in the DB
-//     if (req.body.role === 'CLIENT') {
-//         const { error } = registerClientValidation(req.body);
-//         if (error) {
-//             return resp.status(400).send({ message: error.details[0].message });
-//         }
-//     } else { // USER
-//         const { error } = registerUserValidation(req.body);
-//         if (error) {
-//             return resp.status(400).send({ message: error.details[0].message });
-//         }
-//     }
+    // Validate DATA before to be inserted in the DB
+    const { error } = clinicalStudiesValidation(req.body);
+    if (error) {
+        return resp.status(400).send({ message: error.details[0].message });
+    }
 
-//     try {
-//         // Validate if User can update and the email doesnt exists for another user
-//         const user = await User.findOne({ email: req.body.email });
-//         if (user) {
-//             if (user._id.valueOf() !== req.params.id) {
-//                 return resp.status(400).send({ message: 'User already exists.' });
-//             }  
-//         }
+    try {
+        // Validate if User can update and the email doesnt exists for another user
+        const codeExist = await Studies.findOne({ code: req.body.code });
+        if (codeExist) {
+            if (codeExist._id.valueOf() !== req.params.id) {
+                return resp.status(400).send({ message: 'Code already exists.' });
+            }  
+        }
 
-//         // // BEFORE UPDATE PASSWORD .. VALIDATING OLD PASS AND UPDATE WITH NEW ONE
-//         // // Validate Password
-//         // const validPassword = await bcrypt.compare(req.body.oldPassword, user.password);
-
-//         // if (!validPassword) {
-//         //     return resp.status(401).send({ message: 'Incorrect Old Password' });
-//         // }
-
-//         const obj = {
-//             email: req.body.email,
-//             name: req.body.name,
-//             lastName: req.body.lastName,
-//             role: req.body.role,
-//             code: req.body.code,
-//             address: req.body.address,
-//             contactNumber: req.body.contactNumber,
-//             neighborhood: req.body.neighborhood,
-//             city: req.body.city,
-//             zipCode: req.body.zipCode,
-//             active: true,
-//             passReset: true
-//         }
-//         if (req.body.password != null) {
-//             // Hash password
-//             const salt = await bcrypt.genSalt(10);
-//             const hashedPassword = await bcrypt.hash(req.body.password, salt);
-//             obj.password = hashedPassword;
-//         }
+        // create new clinical study
+        const clinical = new Studies({
+            code: req.body.code,
+            name: req.body.name,
+            referenceValues: req.body.referenceValues,
+            cost: req.body.cost,
+            description: req.body.description,
+            active: true
+        });
         
-//         const updateUser = await User.updateOne(
-//             { _id: req.params.id },
-//             { $set: obj }
-//         );
+        const updateStudy = await Studies.updateOne(
+            { _id: req.params.id },
+            { $set: obj }
+        );
 
-//         return resp.status(200).json(updateUser);
-//     }catch(err){
-//         console.log(err);
-//         return resp.status(500).json({ message: err });
-//     } 
-// });
+        return resp.status(200).json(updateStudy);
+    }catch(err){
+        console.log(err);
+        return resp.status(500).json({ message: err });
+    } 
+});
 
 module.exports = router;
